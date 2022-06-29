6aa1b41397cc918d73f01858a4e32872830b2882
vladislav.tabala <vladislav.tabala@x2sy.com>
2022-06-30 00:42:16 +0300

    feat: auth, user

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -7,46 +7,14 @@ import '../models/http_exception.dart';
 import './product.dart';
 
 class Products with ChangeNotifier {
-  List<Product> _items = [
-    // Product(
-    //   id: 'p1',
-    //   title: 'Red Shirt',
-    //   description: 'A red shirt - it is pretty red!',
-    //   price: 29.99,
-    //   imageUrl:
-    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    // ),
-    // Product(
-    //   id: 'p2',
-    //   title: 'Trousers',
-    //   description: 'A nice pair of trousers.',
-    //   price: 59.99,
-    //   imageUrl:
-    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    // ),
-    // Product(
-    //   id: 'p3',
-    //   title: 'Yellow Scarf',
-    //   description: 'Warm and cozy - exactly what you need for the winter.',
-    //   price: 19.99,
-    //   imageUrl:
-    //       'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    // ),
-    // Product(
-    //   id: 'p4',
-    //   title: 'A Pan',
-    //   description: 'Prepare any meal you want.',
-    //   price: 49.99,
-    //   imageUrl:
-    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    // ),
-  ];
-  // var _showFavoritesOnly = false;
+  List<Product> _items = [];
+
+  final String authToken;
+  final String userId;
+
+  Products(this.authToken, this.userId, this._items);
 
   List<Product> get items {
-    // if (_showFavoritesOnly) {
-    //   return _items.where((prodItem) => prodItem.isFavorite).toList();
-    // }
     return [..._items];
   }
 
@@ -58,24 +26,31 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  // void showFavoritesOnly() {
-  //   _showFavoritesOnly = true;
-  //   notifyListeners();
-  // }
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    var params = {'auth': authToken};
 
-  // void showAll() {
-  //   _showFavoritesOnly = false;
-  //   notifyListeners();
-  // }
+    if (filterByUser) {
+      params['orderBy'] = "creatorId";
+      params['equalTo'] = userId;
+    }
 
-  Future<void> fetchAndSetProducts() async {
-    final url = Uri.https('shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app', '/products.json');
+    var url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products.json',
+        params);
+
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       if (extractedData == null) {
         return;
       }
+      url = Uri.https(
+          'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+          '/userFavorites/$userId.json',
+          {'auth': authToken});
+      final favoriteResponse = await http.get(url);
+      final favoriteData = json.decode(favoriteResponse.body);
       final List<Product> loadedProducts = [];
       extractedData.forEach((prodId, prodData) {
         loadedProducts.add(Product(
@@ -83,7 +58,8 @@ class Products with ChangeNotifier {
           title: prodData['title'],
           description: prodData['description'],
           price: prodData['price'],
-          isFavorite: prodData['isFavorite'],
+          isFavorite:
+              favoriteData == null ? false : favoriteData[prodId] ?? false,
           imageUrl: prodData['imageUrl'],
         ));
       });
@@ -95,7 +71,10 @@ class Products with ChangeNotifier {
   }
 
   Future<void> addProduct(Product product) async {
-    final url = Uri.https('shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app', '/products.json');
+    final url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products.json',
+        {'auth': authToken});
     try {
       final response = await http.post(
         url,
@@ -104,7 +83,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
-          'isFavorite': product.isFavorite,
+          'creatorId': userId,
         }),
       );
       final newProduct = Product(
@@ -115,7 +94,7 @@ class Products with ChangeNotifier {
         id: json.decode(response.body)['name'],
       );
       _items.add(newProduct);
-      // _items.insert(0, newProduct); // at the start of the list
+
       notifyListeners();
     } catch (error) {
       print(error);
@@ -126,7 +105,10 @@ class Products with ChangeNotifier {
   Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
-      final url = Uri.https('shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app', '/products/$id.json');
+      final url = Uri.https(
+          'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+          '/products/$id.json',
+          {'auth': authToken});
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -142,7 +124,10 @@ class Products with ChangeNotifier {
   }
 
   Future<void> deleteProduct(String id) async {
-    final url = Uri.https('shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app', '/products/$id.json');
+    final url = Uri.https(
+        'shop-app-3b80e-default-rtdb.europe-west1.firebasedatabase.app',
+        '/products/$id.json',
+        {'auth': authToken});
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
     _items.removeAt(existingProductIndex);
